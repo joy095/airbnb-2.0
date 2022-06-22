875a332d89769c6819a51e8a601cb6fe68e06913
Joy Karmakar <96533682+joy095@users.noreply.github.com>
2022-06-22 20:59:31 +0530

    Add files via upload

--- search.js
+++ search.js
@@ -11,9 +11,9 @@ function Search({ searchResults }) {
 
   const { location, startDate, endDate, noOfGuests } = router.query;
 
-  const formattedStartDate = format(new Date(startDate), "dd MMMM yy");
-  const formattedEndDate = format(new Date(endDate), "dd MMMM yy");
-  const range = `${formattedStartDate} - ${formattedEndDate}`;
+  // const formattedStartDate = format(new Date(startDate), "dd MMMM yy");
+  // const formattedEndDate = format(new Date(endDate), "dd MMMM yy");
+  // const range = `${formattedStartDate} - ${formattedEndDate}`;
 
   return (
     <div>
@@ -22,7 +22,7 @@ function Search({ searchResults }) {
       <main className="flex">
         <section className="flex-grow pt-14 px-6">
           <p className="text-xs">
-            300+ Stays - {range} - for {noOfGuests} guests
+            300+ Stays - {} - for {noOfGuests} guests
           </p>
 
           <h1 className="text-3xl font-semibold mt-2 mb-6">
